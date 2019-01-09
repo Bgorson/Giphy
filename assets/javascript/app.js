6c8ee919634d4f763adb6bd3cd6855823b9c63cf
BGorson <BGorson32@gmail.com>
2019-01-08 23:40:53 -0600

    Major progress. Mostly need to just clean up formating and qol

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,72 @@
-
-
 var nameQuery;
 var staticQueryURL;
 var activeQueryURL;
-
-var topics = ["Lucille", "Buster", "Michael", "Lindsey","Gob","Blue Man", "Tobias"];
+var topics = ["Lucille", "Buster", "Michael", "Lindsey","Gob","Blue Man", "Tobias", "George Michael"];
 
 //Create buttons based on topics
+function createButtons(){
+  $('#topics').html('')
 for (i=0; i<topics.length;i++){
     var buttons = $('<button>');
-    buttons.addClass("topics");
+    buttons.addClass("topics btn btn-primary");
     buttons.html(topics[i]);
     buttons.attr("name", topics[i]); 
     buttons.appendTo('#topics'); 
   }
+}
 
 //click event to populate page with static GIFS with Rating
-$(".topics").on("click", function(){
-    $("#gifs").html("");
-    nameQuery = $(this).attr("name");
-    staticQueryURL= "http://api.giphy.com/v1/gifs/search?q=" + nameQuery + "-arrested-development&api_key=AX02ZMKDt1EVKnwZGVJUoOEhJQxOW6ol"
-    console.log(staticQueryURL);
-    console.log(nameQuery)
-   
-    
-    $.ajax({
-        url: staticQueryURL,
-        method: "GET"
-      }).then(function(response) {
-        for (var i=0; i < 10; i++) {
-          var img = $("<img>");
-          img.addClass("gifImage")
-          img.attr("src", response.data[i].images.fixed_height_still.url);
-          img.attr("number",i);
-          img.attr("activeURL",response.data[i].images.fixed_height.url);
-          img.attr("isActive",false);
-          $("#gifs").append(img);
-        }
-})
+$(document).on("click", ".topics", function() {
+   populateGifs(this)
 })
 
+function populateGifs(element){
+  $("#gifs").html("");
+  nameQuery = $(element).attr("name");
+  console.log(element)
+  staticQueryURL= "http://api.giphy.com/v1/gifs/search?q=" + nameQuery + "-arrested-development&api_key=AX02ZMKDt1EVKnwZGVJUoOEhJQxOW6ol"
+  $.ajax({
+      url: staticQueryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response)
+      for (var i=0; i < 10; i++) {
+        var gifDiv= $("<div class = 'gifDiv mx-2 my-1'>");
+        var img = $("<img>");
+        var ratingText = $("<p>")
+        img.addClass("gifImage")
+        img.attr("src", response.data[i].images.fixed_height_still.url);
+        img.attr("activeURL",response.data[i].images.fixed_height.url);
+        var rating = response.data[i].rating
+        ratingText.html("Rating:" + rating);
+        $(gifDiv).append(ratingText)
+        $(gifDiv).append(img);
+        $("#gifs").append(gifDiv)
+           
+      }
+})
+}
 
 //Click event to activate GIF and de-activate 
-$(".gifImage").on("click",function() {
-console.log("clicked")
+$(document).on("click", ".gifImage", function() {
+selectedGif=$(this);
+  var animated= selectedGif.attr("activeurl");
+  var static = selectedGif.attr("src");
+  selectedGif.attr("src",animated);
+  selectedGif.attr("activeurl",static);
 
 })
-
-
-
-/* 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    var gs = $(".giphy-stuff");
-    for (var i=0; i < 5; i++) {
-      var img = $("<img>").attr("src", response.data[i].images.original.url)
-      console.log(response.data[i])
-      $(gs).append(img)
-    }
-
-
-    Create and save references to 3 td elements containing the Title, Year, and Actors from the AJAX response object
-    var img = $("<img>").text(response.data.url)
-  Append the td elements to the new table row
-  $(newgif).append(img);
- 
-  Append the table row to the tbody element
-  $("body").append(newgif)
-  });  */
+// This function handles events where one button is clicked
+$("#add-character").on("click", function() {
+  event.preventDefault();
+  var character = $("#character-input").val().trim();
+  //Clears input field
+  $("#character-input").val('');
+       //Checks if its been added
+      if (character.indexOf(topics) == -1){
+  topics.push(character);
+  console.log(topics)
+  createButtons();
+      }
+})
+createButtons()
